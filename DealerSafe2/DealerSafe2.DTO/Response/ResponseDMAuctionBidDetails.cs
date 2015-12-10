850b1f217bf3f620f9ec93ba499bae779f8103c6
Bülent Keskin <bulentkeskin@gmail.com>
2015-12-10 11:09:44 +0200

    office

---
+++ DealerSafe2/DealerSafe2.DTO/Response/ResponseDMAuctionBidDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DealerSafe2.DTO.Response
+{
+    public class ResponseDMAuctionBidDetails
+    {
+        public string Id {get;set;}
+        public int BiggestBid {get;set;}
+        public int MinimumBidPrice {get;set;}
+        public int MinimumBidInterval {get;set;}
+        public string DomainName {get;set;}
+        public int BuyItNowPrice { get; set; }
+    }
+}
